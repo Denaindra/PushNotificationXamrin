6920e5c82ca622e8ced4ab1dd59b28241965cc95
gadperera <gadperera@virtusa.com>
2019-02-15 15:16:44 +0530

    add appcenter tracking events

--- PushNotificationXamrin/MainPage.xaml.cs
+++ PushNotificationXamrin/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AppCenter.Analytics;
 using Xamarin.Forms;
 
 namespace PushNotificationXamrin
@@ -12,6 +13,23 @@ namespace PushNotificationXamrin
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        void DepositClicked(object sender, System.EventArgs e)
+        {
+             Analytics.TrackEvent("Video clicked", new Dictionary<string, string> {
+                    { "Category"
+                    , "Book" },
+                    { "FileName", "Financing IQ"}
+                });
+        }
+
+        void WidthdarwClicked(object sender, System.EventArgs e)
+        {
+            Analytics.TrackEvent("Widthdarw clicked", new Dictionary<string, string> {
+                    { "Category", "Beverage" },
+                    { "FileName", "Sprite"}
+                });
         }
     }
 }
